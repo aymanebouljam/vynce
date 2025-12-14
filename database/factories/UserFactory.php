ed9c260d3a86b01331ae702b7115bec1ec90a26d
Aymane Bouljam <235529830+bulljam@users.noreply.github.com>
2025-12-14 21:42:04 +0100

    chore: generate slug-safe usernames in the user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,7 +26,7 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
-            'username' => fake()->unique()->userName(),
+            'username' => Str::lower(Str::slug(fake()->unique()->userName(), separator: '')),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
